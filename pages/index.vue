8f92486750427f36bb803b2962bac915832b3b5a
Bukola <sobowalebukola@gmail.com>
2020-01-01 12:54:28 +0100

    Vue custom starter

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+     <article class="pa3 pa5-ns">
+  <h1 class="f4 bold center mw6">Todos</h1>
+  <ul class="list pl0 ml0 center mw6 ba b--light-silver br2"  v-bind="task" >
+    <li v-for=" todo of todos" 
+    class="ph3 pv3 bb b--light-silver"
+    v-bind:key ="todo"
+    >
+    {{todo.task}}     
+ </li>
+  </ul>
+</article>
+  </div>
+</template>
+<script>
+import {mapState, mapMutations} from "vuex"
+export default {
+  computed: {
+      ...mapState({
+        todos: state =>state.todos
+      })
+    },
+  methods:{
+     
+    }
+}
+</script>
---
+++ store/index.js
@@ -0,0 +1,11 @@
+export const state = {
+    todos: [
+        {task:"eat"},
+        {task:"sleep"},
+        {task: "code"}
+]
+}
+
+export const mutations = {
+   
+}
